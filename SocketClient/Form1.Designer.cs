2cc5775e792b44eaf70d23e48d67666a0d91f2e8
mbaibatyr <murat_b@mail.ru>
2025-05-23 20:15:48 +0500

    ++

---
+++ SocketClient/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(102, 304);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 23);
+            this.tbPort.TabIndex = 6;
+            this.tbPort.Text = "8080";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(102, 275);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(100, 23);
+            this.tbHost.TabIndex = 5;
+            this.tbHost.Text = "127.0.0.1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 229);
+            this.listBox1.TabIndex = 4;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(100, 348);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(183, 23);
+            this.tbMessage.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Send Socket";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tbPort;
+        private TextBox tbHost;
+        private ListBox listBox1;
+        private TextBox tbMessage;
+        private Button button1;
+    }
+}
---
+++ SocketClient/Form1.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async Task SendToServer()
+        {
+            await Task.Run(() =>
+            {
+                byte[] bytes = new byte[1024];
+                var ip = Dns.GetHostEntry(tbHost.Text);
+                var ad = ip.AddressList[0];
+                var ep = new IPEndPoint(ad, int.Parse(tbPort.Text));
+                Socket sender = new Socket(ad.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(ep);
+                byte[] msg = Encoding.UTF8.GetBytes(tbMessage.Text);
+                int count = sender.Send(msg);
+                count = sender.Receive(bytes);
+                listBox1.Items.Add("Получено от сервера " + Encoding.UTF8.GetString(bytes, 0, count));
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendToServer();
+        }
+    }
+}
