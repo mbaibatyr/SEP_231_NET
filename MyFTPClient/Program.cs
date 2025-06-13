09288d4ad85d9e39f9a29a06a248c3422a2d3707
MBaibatyrov <MBaibatyrov@beeline.kz>
2025-06-13 21:40:48 +0500

    ++

--- MyFTPClient/Program.cs
+++ MyFTPClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FluentFTP;
+using Renci.SshNet;
 
 namespace MyFTPClient
 {
@@ -13,10 +14,36 @@ namespace MyFTPClient
             client.Host = url;
             client.Credentials = new System.Net.NetworkCredential(user, password);
             client.Connect();
+            //client.UploadFile("123.txt", "")
+            var result = client.DownloadFile("123.jpg", "/10.101.1.2_20250613-12024731_IVA.jpg");
+            //foreach (var item in client.GetListing("/"))
+            //{
+            //    Console.WriteLine(item.FullName);
+            //}
+        }
+
+
+        static void SFtpTest()
+        {
+            string host = "test.rebex.net";
+            int port = 22;
+            string user = "demo";
+            string password = "password";
+            using (var client = new SftpClient(host, port, user, password))
+            {
+                client.Connect();
+                var files = client.ListDirectory("/");
+                //foreach (var item in files)
+                //{
+                //    Console.WriteLine(item.FullName);
+                //}                
+                client.Disconnect();
+            }
+
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SFtpTest();
         }
     }
 }
---
+++ MySMTP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mail;
+
+namespace MySMTP
+{
+    internal class Program
+    {
+
+        static void Send()
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("murat_b@mail.ru");
+            mail.To.Add("ernar30@gmail.com");
+            mail.Subject = "Test Email";
+            mail.Body = "This is a test email sent from MySMTP application.";
+            Attachment attachment = new Attachment(@"D:\Projects\SEP_231_NET\MyFTPClient\bin\Debug\net7.0\123.txt");
+            mail.Attachments.Add(attachment);
+            using (SmtpClient client = new SmtpClient("smtp.mail.ru", 587))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("murat_b@mail.ru", "");
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mail);
+                Console.WriteLine("sent");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Send();
+        }
+    }
+}
